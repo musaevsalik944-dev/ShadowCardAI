ee63616602ce4a0c8124ec0c91f242683374a075
musaevsalik944-dev <musaevsalik944@gmail.com>
2026-08-17 14:16:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/generate", async (req, res) => {
+
+  const { prompt } = req.body;
+
+  console.log("Запрос от ShadowCard AI:", prompt);
+
+
+  res.json({
+    message: "Сервер получил запрос",
+    prompt: prompt
+  });
+
+});
+
+
+app.listen(3000, () => {
+  console.log("ShadowCard AI server запущен");
+});
